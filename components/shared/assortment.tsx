72784cb75d13655f996e92aa6330d030339be418
Konovalova-Kateryna <kytana2112@gmail.com>
2026-03-26 21:59:53 +0200

    fix bugs of build

--- components/shared/assortment.tsx
+++ components/shared/assortment.tsx
@@ -40,7 +40,7 @@ const listVariants = {
 
 const itemVariants = {
   hidden: { opacity: 0, y: 20 },
-  visible: { opacity: 1, y: 0, transition: { duration: 0.4, ease: "easeOut" } },
+  visible: { opacity: 1, y: 0, transition: { duration: 0.4, ease: [0.25,0.1,0.25,1]as const} },
   exit: { opacity: 0, scale: 0.95, transition: { duration: 0.2 } },
 };
 
